7dbcbd1eb00460d20546c06992ed6aa11b462cf2
gujiafeng <7837763+Gjiafeng@user.noreply.gitee.com>
2021-06-04 15:53:23 +0800

    。。

---
+++ src/test/java/pers/gjf/toolutils/ToolutilsApplicationTests.java
@@ -0,0 +1,15 @@
+package pers.gjf.toolutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolutilsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
